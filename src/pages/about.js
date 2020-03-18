f1c2b4b2d255c413d1d9bd94b2d0799819e08ecb
Dragomir Dan <dan@dragomirweb.com>
2020-03-18 18:48:06 +0200

    remove react glide

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,18 +4,11 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import { Glide } from 'react-glide'
-import 'react-glide/lib/reactGlide.css';
 
 const AboutPage = () => (
   <Layout>
     <SEO title="About" />
     <div>
-    <Glide height={500} dots={false} autoplay={true}>
-      <img src='https://picsum.photos/500/500' />
-      <img src='https://picsum.photos/500/500' />
-      <img src='https://picsum.photos/500/500' />
-    </Glide>
     </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
